37e856dbc5e8018ba0759103186b78177abcef4f
rprabhakar789 <rprabhakar789@users.noreply.github.com>
2022-02-21 12:33:13 +0000

    Deploying to gh-pages from  @ 79dac7d03bf7aa3b8c5ee9cc517c7948446699a3 🚀

---
+++ precache-manifest.deabbcfdf5ababddd180b0b81b9fb11e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c213ad9bf93e28d729160ee17990b74e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "802ddc2fb346849ff8dd",
+    "url": "/portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "88cad3078b026431528d",
+    "url": "/portfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "32fe34ef8589ad709cf8",
+    "url": "/portfolio/static/css/main.e9fa0e1b.chunk.css"
+  },
+  {
+    "revision": "92cbccf5d12a4f16a671",
+    "url": "/portfolio/static/js/2.bde09400.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio/static/js/2.bde09400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "802ddc2fb346849ff8dd",
+    "url": "/portfolio/static/js/3.6239bc56.chunk.js"
+  },
+  {
+    "revision": "88cad3078b026431528d",
+    "url": "/portfolio/static/js/4.7d5fb721.chunk.js"
+  },
+  {
+    "revision": "32fe34ef8589ad709cf8",
+    "url": "/portfolio/static/js/main.c4fa6553.chunk.js"
+  },
+  {
+    "revision": "f39d0465f5e60e2b841f",
+    "url": "/portfolio/static/js/runtime-main.c6ace319.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "264de7a7463c58e4ed0edcb14a35fa71",
+    "url": "/portfolio/static/media/Logo_2.264de7a7.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d3a492b317f817781e3dc09bcd16f70b",
+    "url": "/portfolio/static/media/Quiz Portal-logos.d3a492b3.jpeg"
+  },
+  {
+    "revision": "cb9c5bcf871ec94bf1315ed8b2e0c759",
+    "url": "/portfolio/static/media/brickbreaker.cb9c5bcf.png"
+  },
+  {
+    "revision": "fc57905da8d57141cff8cdc7c76f99eb",
+    "url": "/portfolio/static/media/cap.fc57905d.jpg"
+  },
+  {
+    "revision": "76e40785f418f61a108ffd0bbe65511c",
+    "url": "/portfolio/static/media/codersbit.76e40785.jpg"
+  },
+  {
+    "revision": "ad52489a8899de074b66c90a36fee79e",
+    "url": "/portfolio/static/media/coding-1.ad52489a.gif"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "3b702732144121a568187011d65c0b29",
+    "url": "/portfolio/static/media/hibernate.3b702732.svg"
+  },
+  {
+    "revision": "85a78ea0003f050b89a3d931f47a37e2",
+    "url": "/portfolio/static/media/myImg.85a78ea0.jpg"
+  },
+  {
+    "revision": "b1a921d3b95c602c3713d497d77f467c",
+    "url": "/portfolio/static/media/operating-system.b1a921d3.jpg"
+  },
+  {
+    "revision": "df337fb86f13dd3c8bea66739440704d",
+    "url": "/portfolio/static/media/rahul-laptop1.df337fb8.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "b80103753f499861a07c28189913dfd1",
+    "url": "/portfolio/static/media/sneaker.b8010375.jpg"
+  },
+  {
+    "revision": "13f6ad8391a8ad579f70431ef51c14a1",
+    "url": "/portfolio/static/media/spring-framework-icon.13f6ad83.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.34c301aae91226284a6da52cbb93382b.js"
+  "/portfolio/precache-manifest.deabbcfdf5ababddd180b0b81b9fb11e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
